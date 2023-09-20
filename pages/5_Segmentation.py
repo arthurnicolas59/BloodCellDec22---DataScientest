32f253881ec01e05adc85f1b801b7189b628464a
ludoroch <128477217+ludoroch@users.noreply.github.com>
2023-09-20 21:36:49 +0200

    Changes to be committed:

---
+++ pages/5_Segmentation.py
@@ -0,0 +1,143 @@
+import streamlit as st
+import numpy as np
+import os
+import glob
+import cv2
+from keras import callbacks
+import pandas as pd
+import skimage.io as io
+import skimage.transform as trans
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from keras.models import *
+from keras.layers import *
+from keras.optimizers import *
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras import backend as keras
+from keras.applications import VGG16
+from keras.layers import Multiply, Flatten, Dense, Input
+from keras.models import Model
+from keras.layers import BatchNormalization, Dropout
+from tensorflow.keras.utils import plot_model
+from tensorflow.keras import layers, models
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
+
+st.set_page_config(
+    page_title="Segmentation",
+    page_icon="📊",
+)
+
+#Segmentation
+st.header("Segmentation")
+
+tab1, tab2 = st.tabs(["Segmentation SAM", "Prédiction Unet"])
+
+with tab1:
+    col1,col2=st.columns([0.5,0.5])
+    with col1 :
+        st.subheader("Sélection d'une zone d'intérêt")
+        
+    with col2:
+        st.subheader("Extraction de l'objet")
+    st.image('streamlit_media\SAM.png')
+
+with tab2:
+    st.subheader('Prédiction Unet')
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    df_mask=pd.read_csv('Dataframe\df_mask_SAM.csv')
+
+    ### Définition d'une fonction de perte : Coefficient de Dice
+    def LossDice(y_true, y_pred):
+        numerateur  =tf.reduce_sum(y_true*y_pred, axis=(1, 2))
+        denominateur=tf.reduce_sum(y_true+y_pred, axis=(1, 2))
+        dice=2*numerateur/(denominateur+1E-4)
+        return 1-dice
+
+    def DiceCoeff(y_true, y_pred):
+        numerateur  =tf.reduce_sum(y_true*y_pred, axis=(1, 2))
+        denominateur=tf.reduce_sum(y_true+y_pred, axis=(1, 2))
+        dice=2*numerateur/(denominateur+1E-4)
+        return dice
+
+    ### Création du jeu d'entrainement, de validation et de test
+
+    height=256
+    width=256
+    batch_size=32
+    seed=42
+
+    #### séparation du jeu de données
+    from sklearn.model_selection import train_test_split
+
+    filepaths = df_mask['filepath'].values
+    maskpaths = df_mask['maskpath'].values
+
+    train_filepaths, test_filepaths, train_maskpaths, test_maskpaths = train_test_split(filepaths, maskpaths, test_size=0.2, random_state=seed)
+    train_filepaths, val_filepaths, train_maskpaths, val_maskpaths = train_test_split(train_filepaths, train_maskpaths, test_size=0.2, random_state=seed)
+
+
+    nb_img_train=len(train_filepaths)
+    nb_img_val=len(val_filepaths)
+    nb_img_test=len(test_filepaths)
+
+    ### Chargement éventuel d'un modèle enregistré
+
+    custom_objects = {'DiceCoeff': DiceCoeff, 'LossDice': LossDice}
+    saved_segmenter = tf.keras.models.load_model(
+        r'C:\Users\lrochette\Documents\Perso\DataScientist\BloodCellDec22---DataScientest\Model\model_unet_with_data_augmented\model_unet_with_data_augmented.h5', 
+        custom_objects=custom_objects
+    )
+
+    import tensorflow as tf
+    def load_image(filepath,resize=(256,256)):
+        # Charger l'information brute en mémoire
+        im = tf.io.read_file(filepath)
+        # Décoder l'information en un tensorflow RGB (3 channels).
+        im = tf.io.decode_jpeg(im, channels=3)
+        #Redimensionner l'image
+        return tf.image.resize(im, size=resize)
+
+    def load_mask(filepath,resize=(256,256)):
+        im = tf.io.read_file(filepath)
+        ### Dans le cas des masques, l'image est en noir et blanc, il n'y a donc qu'une valeur par pixel
+        im = tf.io.decode_png(im, channels=1)
+        return tf.image.resize(im, size=resize)
+
+    ### Affichage d'une prédiction de masque en comparaison avec un masque
+    ### fait à partir de l'image segmentée du jeu de donnée Test
+
+    size=2
+    indexes=np.random.choice(len(test_filepaths),size=2)
+
+    i=0
+    for idx in indexes:
+        image=load_image(test_filepaths[idx])
+        img=tf.reshape(image,(1,256,256,3))
+        mask_pred=saved_segmenter.predict(img)
+        mask_true=load_mask(test_maskpaths[idx])
+        image=tf.cast(image,dtype=tf.int32)
+
+        plt.figure(figsize=(15,7))
+
+        i+=1
+        plt.subplot(size,3,i)
+        plt.imshow(image)
+        plt.axis("off")
+        plt.title("Image d'origine")
+
+        i+=1            
+        plt.subplot(size,3,i)
+        plt.imshow(tf.reshape(mask_true,(256,256)),cmap='gray')
+        plt.axis("off")
+        plt.title("Masque issu de SAM")
+
+        i+=1
+        plt.subplot(size,3,i)
+        plt.imshow(tf.reshape(mask_pred,(256,256)),cmap='gray')
+        plt.axis("off")
+        plt.title("Masque predit par le modèle Unet")
+
+        st.pyplot()

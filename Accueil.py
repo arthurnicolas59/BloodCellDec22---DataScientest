5135d397699d3fc65a4bd028ed7aa0f5463841d1
ludoroch <128477217+ludoroch@users.noreply.github.com>
2023-09-29 10:26:53 +0200

    Changes to be committed: 	modified:   Accueil.py

--- Accueil.py
+++ Accueil.py
@@ -8,8 +8,11 @@ st.set_page_config(
 
 # Streamlit app title
 st.title("Projet DataScientest : Classification de cellules sanguines - Dec 2022")
-st.subheader('Ludovic Rochette - Aurélie Amira - Arthur Nicolas - Virginie Belleville')
-
+col1,col2=st.columns([0.8,0.2])
+with col1:
+    st.subheader('Aurélie Amira - Virginie Belleville Bonnefoy Arthur Nicolas - Ludovic Rochette')
+with col2 :
+    st.write("")
 #photo
 col1, col2, col3 = st.columns([0.05,0.9,0.05])
 with col1:
